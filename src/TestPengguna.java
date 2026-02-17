44e71b8ec62c808b7fa29af6c46883fa8a281f89
Dodo Fansyuri Andrian <dotelabruh11@gmail.com>
2026-02-17 22:04:18 +0700

    implement main method for object testing

---
+++ src/TestPengguna.java
@@ -0,0 +1,14 @@
+
+public class TestPengguna {
+    public static void main(String[] args) {
+        
+        Pengguna pekerja = new Pengguna();
+
+        pekerja.setNama("Budi Santoso");
+        pekerja.id = 1001;
+        pekerja.umur = 25;
+
+        pekerja.tampilkanInfo();
+
+    }
+}
